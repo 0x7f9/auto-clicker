5fe9c795c4f73f16bf44eb7d108af522f7c99063
0x7f9 <waterskating@proton.me>
2023-08-25 20:44:16 +1200

    changes to clicker

--- clicker.py
+++ clicker.py
@@ -1,43 +1,45 @@
-import time as t
-import pyautogui
+import time
+import pyautogui as auto
 import keyboard
 import random
 import threading
 
 '''
 pip install pyautogui keyboard
-Base speed of 18.8CPS
+base speed of 19.2 CPS with two threads - https://www.rapidtables.com/
 '''
 class clicker:
     def __init__(self):
         self.clicker = False
-        self.interval = 0
+        self.base = 0
 
-    def toggle_auto_clicker(self):
+    def toggle(self):
         self.clicker = not self.clicker
 
-    def random_number(self):
-        self.interval = random.uniform(0, 0.01) # this will effect you cps
-        return self.interval                    # adding zeros will increase 
-                                                # removing will decrease
+    def number_generate(self):               
+        self.base = random.uniform(0.00025, 0.0005)  # generates a random float between 0.25 ms and 0.5 ms
+        return self.base                             # adding zeros will increase cps
+                                                     # removing will decrease cps 
     def auto_clicker(self):
         while 1:
             if self.clicker:
-                r = self.random_number()
-                pyautogui.click()
-                t.sleep(r)
-                print(r)
+                random_sleep = self.number_generate()
+                auto.click()
+                time.sleep(random_sleep)
+                print(random_sleep)
             else:
-                t.sleep(0.1) # cpu saver -_-
+                time.sleep(0.1) # cpu saver -_-
 
 # main runner
 if __name__ == '__main__':
     c = clicker()
     # hotkey to toggle on and off
-    keyboard.add_hotkey("9", c.toggle_auto_clicker)
-    # start number gen
-    n_gen = threading.Thread(target=c.auto_clicker)
-    n_gen.start()
-    # start clicker
-    c_one = threading.Thread(target=c.auto_clicker)
-    c_one.start()
+    keyboard.add_hotkey("9", c.toggle)
+
+    # start clicker one
+    one = threading.Thread(target=c.auto_clicker)
+    one.start()
+
+    # start clicker two
+    two = threading.Thread(target=c.auto_clicker)
+    two.start()
